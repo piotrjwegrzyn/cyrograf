6367ec439179a7e734275ff948b4ca6ee4784384
Piotr Węgrzyn <piotrwegrzyn@protonmail.com>
2024-08-05 21:43:52 +0200

    eth/ contract, unit tests & pipelines

---
+++ eth/cyrograf.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.26;
+
+contract Cyrograf {
+    struct RGB {
+        uint8 x;
+        uint8 y;
+        uint8 z;
+    }
+
+    struct Background {
+        address author;
+        RGB topLeft;
+        RGB topRight;
+    }
+
+    Background currentBackground;
+
+    constructor() {
+        currentBackground = Background(msg.sender, RGB(0,0,0), RGB(255,255,255));
+
+    }
+
+    function getBackground() public view returns (Background memory){
+        return currentBackground;
+    }
+
+    function setBackground(RGB memory northWest, RGB memory southEast) external {
+        currentBackground.author = msg.sender;
+        currentBackground.topLeft = northWest;
+        currentBackground.topRight = southEast;
+    }
+}
---
+++ eth/tests/cyrograf_test.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+        
+pragma solidity >=0.8.26;
+
+import "remix_tests.sol"; 
+import "remix_accounts.sol";
+import "../cyrograf.sol";
+
+contract CyrografTestSuite {
+    Cyrograf cyrograf;
+
+    // Setup function to initialize the contract before tests run
+    function beforeEach() public {
+        cyrograf = new Cyrograf();
+    }
+
+    function testInitialBackground() public {
+        Cyrograf.Background memory got = cyrograf.getBackground();
+
+        Assert.equal(got.author, address(this), "The author should be the deployer");
+
+        Assert.equal(got.topLeft.x, uint8(0), "Top left X should be 0");
+        Assert.equal(got.topLeft.y, uint8(0), "Top left Y should be 0");
+        Assert.equal(got.topLeft.z, uint8(0), "Top left Z should be 0");
+
+        Assert.equal(got.topRight.x, uint8(255), "Top right X should be 255");
+        Assert.equal(got.topRight.y, uint8(255), "Top right Y should be 255");
+        Assert.equal(got.topRight.z, uint8(255), "Top right Z should be 255");
+    }
+
+    function testSetAndGetBackground() public {
+        cyrograf.setBackground(Cyrograf.RGB(128, 64, 32), Cyrograf.RGB(200, 150, 100));
+
+        Cyrograf.Background memory got = cyrograf.getBackground();
+
+        Assert.equal(got.author, address(this), "The author should be the caller");
+
+        Assert.equal(got.topLeft.x, uint8(128), "North west X should be updated");
+        Assert.equal(got.topLeft.y, uint8(64), "North west Y should be updated");
+        Assert.equal(got.topLeft.z, uint8(32), "North west Z should be updated");
+
+        Assert.equal(got.topRight.x, uint8(200), "South east X should be updated");
+        Assert.equal(got.topRight.y, uint8(150), "South east Y should be updated");
+        Assert.equal(got.topRight.z, uint8(100), "South east Z should be updated");
+    }
+}
+    
